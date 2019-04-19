1db0b433ed11981a30be711965da35d92931e754
David Blümel <bludac16@htl-kaindorf.ac.at>
2019-04-19 16:48:04 +0200

    Implemented class: KontoBenutzer

--- src/main/java/Benutzer/KontoBenutzer.java
+++ src/main/java/Benutzer/KontoBenutzer.java
@@ -6,13 +6,18 @@
 package Benutzer;
 
 import BL.Konto;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author David
  */
-public class KontoBenutzer extends Thread{
+public class KontoBenutzer extends Thread {
+
     private Konto konto;
+    Random rand = new Random();
 
     public KontoBenutzer(Konto konto) {
         this.konto = konto;
@@ -20,8 +25,36 @@ public class KontoBenutzer extends Thread{
 
     @Override
     public void run() {
-        
+        while (true) {
+            int amount = 10 + rand.nextInt(40);
+            if (1 + rand.nextInt(1) == 1) {
+                synchronized (konto) {
+                    if ((konto.getBalance() - amount) >= 0) {
+                        konto.withdraw(amount);
+                        konto.notifyAll();
+                        System.out.format("%s makes withdraw: %d\n", Thread.currentThread().getName(), amount);
+                    } else {
+                        try {
+                            konto.wait();
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(KontoBenutzer.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }
+
+            } else {
+                synchronized (konto) {
+                    konto.deposit(amount);
+                    konto.notifyAll();
+                    System.out.format("%s makes deposit: %d\n", Thread.currentThread().getName(), amount);
+                }
+            }
+            try {
+                Thread.sleep(1 + rand.nextInt(999));
+            } catch (InterruptedException ex) {
+                Logger.getLogger(KontoBenutzer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
-    
-    
+
 }
